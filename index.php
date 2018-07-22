310e1702875ea39460842f6de4858dccf8f37a40
JN646 <josh.ginn@hotmail.co.uk>
2018-07-22 21:26:02 +0100

    Added Header Partials.

--- index.php
+++ index.php
@@ -1,15 +1,4 @@
-<!DOCtype html>
-<html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title>Amusement Manangement System</title>
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.1/css/all.css" integrity="sha384-O8whS3fhG2OnA5Kas0Y9l3cfpmYjapjI0E4theH4iuMD+pLhbf6JI0jIMfYcK3yZ" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/css/bootstrap.min.css" integrity="sha384-Smlep5jCw/wG7hdkwQ/Z5nLIefveQRIY9nfy6xoR1uRYBtpZgI6339F5dgvm/e9B" crossorigin="anonymous">
-    <link rel="stylesheet" href="style.css">
-    <?php include 'DBConfig.php'; ?>
-    <?php include 'functions.php'; ?>
-  </head>
-  <body>
+<<?php include 'partials/_header.php' ?>
 <div class='fluid-container'>
   <br>
 <div class='col-md-12'>
---
+++ partials/_header.php
@@ -0,0 +1,16 @@
+<!DOCtype html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Amusement Manangement System</title>
+
+    <!-- Stylesheets -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.1/css/all.css" integrity="sha384-O8whS3fhG2OnA5Kas0Y9l3cfpmYjapjI0E4theH4iuMD+pLhbf6JI0jIMfYcK3yZ" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/css/bootstrap.min.css" integrity="sha384-Smlep5jCw/wG7hdkwQ/Z5nLIefveQRIY9nfy6xoR1uRYBtpZgI6339F5dgvm/e9B" crossorigin="anonymous">
+    <link rel="stylesheet" href="../style.css">
+
+    <!-- Includes -->
+    <?php include '../DBConfig.php'; ?>
+    <?php include '../functions.php'; ?>
+  </head>
+  <body>
